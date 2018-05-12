b102064329268b92f62f10553a5fcd427f2f429a
Johana Ramirez <JCRamirez@johanas-mbp.attlocal.net>
2018-05-11 23:15:18 -0700

    working

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,25 +7,51 @@ class Game extends Component {
   state = {
     strangerthings: cards,
     score: 0,
-    randomNumber: 0
+    topScore: 0,
+    message: "Click an image but not twice!", // Default message 
+    clicked: [], // Empty array to keep track of clicked 
   }
 
-  componentDidMount() {
-    let random = Math.floor(Math.random() * 6) + 1;
-    this.setState({
-      randomNumber: random
-    });
-    this.shuffleArray();
-  }
-
-  cardClickedOn = (id) => {
-    if(id === this.state.randomNumber) {
+  cardClickedOn = (clicked) => {
+    if (this.state.clicked.includes(clicked)) { // Includes determines whether an array has a certain element
       this.setState({
-        score: this.state.score + 1
-      });
-    } 
-      this.shuffleArray();
+        score: 0,
+        clicked: [],
+        message: "You clicked an image twice. Try Again."
+      })
+    } else {
+      this.updateScore();
+    }
+    this.state.clicked.push(clicked) // Pushing the clicked into the array
+    this.shuffleArray();
+    this.winner();
   };
+
+  updateScore = () => {
+    if (this.state.score === this.state.topScore) {
+      this.setState({
+        score: this.state.score + 1,
+        topScore: this.state.topScore + 1,
+        message: "Correct, awesome job!"
+      })
+    } 
+    if (this.state.score < this.state.topScore) {
+      this.setState({
+        score: this.state.score + 1,
+        message: "Correct, awesome job!"
+      })
+    }
+  }
+
+  winner = () => {
+    if (this.state.score === 6) { // Only have 6 cards
+      return this.setState({
+        score: 0,
+        clicked: [],
+        message: "Congrats, you won!"
+      })
+    }
+  }
 
   shuffleArray = () => {
     let array = this.state.strangerthings;
@@ -35,30 +61,31 @@ class Game extends Component {
       const temp = array[i];
       array[i] = array[j];
       array[j] = temp;
+    }
+    this.setState({
+      strangerthings: array
+    })
   }
-  this.setState({
-    strangerthings: array
-  })
-}
-  
+
   render() {
     return (
       <div className="container">
-          <h3>Random Number: {this.state.randomNumber}</h3>
-          <h3>Score: {this.state.score}</h3>
-          {this.state.strangerthings.map(cast => (
-           <Card
-            key={cast.id} 
-            id={cast.id} 
-            name={cast.name} 
+        <h3>Score: {this.state.score} </h3>
+        <h3>Top Score: {this.state.topScore} </h3>
+        <h3>Message: {this.state.message} </h3>
+        <div className="container">
+        {this.state.strangerthings.map(cast => (
+          <Card
+            key={cast.id}
+            id={cast.id}
+            name={cast.name}
             image={cast.image}
             cardClickedOn={this.cardClickedOn} />
-          ))} 
+        ))}
+        </div>
       </div>
     );
   }
 }
 
 export default Game;
-
-// {this.state.strangerthings.map(cast => (
